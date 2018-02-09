5044fd024d0d7715cfc81070b50b1d06335b801f
ctnswb <xclai.07@gmail.com>
2018-02-08 21:24:41 -0800

    route tag component displays aspect ratio card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RouteTagComponent } from './routeTag/routeTag.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouteTagComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/routeTag/routeTag.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'route-tag',
+  templateUrl: './routeTag.component.html',
+  styleUrls: ['./routeTag.component.css']
+})
+export class RouteTagComponent {
+
+}
