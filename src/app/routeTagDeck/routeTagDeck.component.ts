806dd95adc0cd349e81d3139ccd13f1f8c7c707d
ctnswb <xclai.07@gmail.com>
2018-02-11 21:26:07 -0800

    removed flex layout, displays cards in a column, added page breaks

--- src/app/routeTagDeck/routeTagDeck.component.ts
+++ src/app/routeTagDeck/routeTagDeck.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'route-tag-deck',
   templateUrl: './routeTagDeck.component.html',
-  styleUrls: ['./routeTagDeck.component.css']
+  styleUrls: ['./routeTagDeck.component.css'],
 })
 
 export class RouteTagDeckComponent {
