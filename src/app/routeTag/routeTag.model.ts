aea0bddfed0f3ffa6024cf95e24d5a13df33c8fe
ctnswb <xclai.07@gmail.com>
2018-02-12 13:17:19 -0800

    build route tag model and route tag list

---
+++ src/app/routeTag/routeTag.model.ts
@@ -0,0 +1,28 @@
+export class RouteTag {
+  public type: string; // boulder or rope
+  public color: string;
+  public routeName: string;
+  public grade: string;
+  public setDate: string;
+  public setter: string;
+
+  //rope only
+  public routeInfo: string; // tr or lead
+  public topRopeNumber: number;
+
+  //optional
+  public routeNotes: string; //arete crack
+
+  constructor(type: string, color: string, routeName:string, grade: string, setDate:string, setter:string, routeInfo: string, topRopeNumber: number, routeNotes:string) {
+    this.type = type;
+    this.color = color;
+    this.routeName = name;
+    this.grade = grade;
+    this.setDate = setDate;
+    this.setter = setter;
+
+    this.routeInfo = routeInfo;
+    this.topRopeNumber = topRopeNumber;
+    this.routeNotes = routeNotes;
+  }
+}
---
+++ src/app/routeTagList/routeTagList.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouteTag } from '../routeTag/routeTag.model';
+
+@Component({
+  selector: 'route-tag-list',
+  templateUrl: './routeTagList.component.html',
+  styleUrls: ['./routeTagList.component.css']
+})
+
+export class RouteTagListComponent {
+  routeTags: RouteTag[] = [
+    new RouteTag('boulder', 'blue', 'I am sleepy', '4', '2/12/18', 'CL', null, null, null),
+    new RouteTag('rope', 'purple', 'Need to take a nap', '11c', '2/12/18', 'CL', 'TR and Lead', 58, null)
+  ];
+
+  printTags: RouteTag[] = [
+    new RouteTag('boulder', 'blue', 'I am sleepy', '4', '2/12/18', 'CL', null, null, null),
+    new RouteTag('rope', 'purple', 'Need to take a nap', '11c', '2/12/18', 'CL', 'TR and Lead', 58, null),
+    new RouteTag('rope', 'red', 'I see this as red not orange', '10d', '2/12/18', 'CC', 'TR', 2, 'Crack on'),
+    new RouteTag('boulder', 'black', 'I dont boulder', '2', '2/12/18', 'ML', null, null, null),
+    new RouteTag('rope', 'pink', 'What colors am I missing', '8', '2/12/18', 'CUZ', 'Lead', null, null)
+  ]
+}
