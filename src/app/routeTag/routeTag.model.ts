47739f04c2866fba7ffef263c632d3d610072ad2
ctnswb <xclai.07@gmail.com>
2018-02-14 21:24:37 -0800

    update tag formatting to fit on index card

--- src/app/routeTag/routeTag.model.ts
+++ src/app/routeTag/routeTag.model.ts
@@ -9,15 +9,17 @@ export class RouteTag {
   //rope only
   public routeInfo: string; // tr or lead
   public topRopeNumber: number;
+  public letter: string; // a b c d
 
   //optional
   public routeNotes: string; //arete crack
 
-  constructor(type: string, color: string, routeName:string, grade: string, setDate:string, setter:string, routeInfo: string, topRopeNumber: number, routeNotes:string) {
+  constructor(type: string, color: string, routeName:string, grade: string, letter: string, setDate:string, setter:string, routeInfo: string, topRopeNumber: number, routeNotes:string) {
     this.type = type;
     this.color = color;
     this.routeName = routeName;
     this.grade = grade;
+    this.letter = letter;
     this.setDate = setDate;
     this.setter = setter;
 
--- src/app/routeTagList/routeTagList.component.ts
+++ src/app/routeTagList/routeTagList.component.ts
@@ -9,15 +9,15 @@ import { RouteTag } from '../routeTag/routeTag.model';
 
 export class RouteTagListComponent {
   routeTags: RouteTag[] = [
-    new RouteTag('boulder', 'blue', 'I am sleepy', '4', '2/12/18', 'CL', null, null, null),
-    new RouteTag('rope', 'purple', 'Need to take a nap', '11c', '2/12/18', 'CL', 'TR and Lead', 58, null)
+    new RouteTag('boulder', 'blue', 'I am sleepy', '4', null, '2/12/18', 'CL', null, null, null),
+    new RouteTag('rope', 'purple', 'Need to take a nap', '11', 'c', '2/12/18', 'CL', 'TR and Lead', 58, null)
   ];
 
   printTags: RouteTag[] = [
-    new RouteTag('boulder', 'blue', 'I am sleepy', '4', '2/12/18', 'DG', null, null, null),
-    new RouteTag('rope', 'purple', 'Need to take a nap', '11c', '2/12/18', 'KS', 'Top Rope #58 and Lead', 58, null),
-    new RouteTag('rope', 'red', 'I see this as red not orange', '10d', '2/12/18', 'CC', 'Top Rope #2 Only', 2, 'Crack on'),
-    new RouteTag('boulder', 'black', 'I dont boulder', '2', '2/12/18', 'ML', null, null, null),
-    new RouteTag('rope', 'pink', 'What colors am I missing', '8', '2/12/18', 'CUZ', 'Lead Only', null, null)
+    new RouteTag('boulder', 'blue', 'I am sleepy', '4', null, '2/12/18', 'DG', null, null, null),
+    new RouteTag('rope', 'purple', 'Need to take a nap', '11', 'c', '2/12/18', 'KS', 'Top Rope #58 and Lead', 58, null),
+    new RouteTag('rope', 'red', 'I see this as red not orange', '10', 'd', '2/12/18', 'CC', 'Top Rope #2 Only', 2, 'Crack on'),
+    new RouteTag('boulder', 'black', 'I dont boulder', '2', null, '2/12/18', 'ML', null, null, null),
+    new RouteTag('rope', 'pink', 'What colors am I missing', '8', null, '2/12/18', 'CUZ', 'Lead Only', null, null)
   ]
 }
